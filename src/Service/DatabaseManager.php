2160ceca3b93b8b8257ce1eaf50562e8965618c8
JTS22 <34191086+JTS22@users.noreply.github.com>
2017-12-13 10:23:25 +0100

    Add files via upload

--- src/Service/DatabaseManager.php
+++ src/Service/DatabaseManager.php
@@ -3,6 +3,8 @@
 namespace GyWa\OrganizerBundle;
 
 use Contao\CoreBundle\Exception\InternalServerErrorException;
+use Contao\FilesModel;
+use Contao\PageModel;
 use Doctrine\DBAL\Connection;
 use Contao\Dbafs;
 use Contao\Database;
@@ -24,23 +26,38 @@ class DatabaseManager
         }
     }
 
+    public function checkForDistinctFileNames() {
+        $duplicateFolders = Database::getInstance()->prepare("select path from tl_files WHERE name in (select name from tl_files where type='folder' AND path LIKE 'files/daten/%' group by name having count(*) > 1) AND type='folder' AND path LIKE 'files/daten/%' ORDER BY name ASC")->execute();
+        return $duplicateFolders;
+    }
+
     public function selectAllRegularPages() {
-        return Database::getInstance()->prepare("SELECT * FROM tl_page WHERE type='regular'")->execute();
+        return Database::getInstance()->prepare("SELECT `id`, `alias` FROM tl_page WHERE `type`='regular'")->execute();
     }
 
     public function requestParentPageForID($id) {
-        if($parentPage = Database::getInstance()->prepare("SELECT id, alias FROM tl_page WHERE id=(SELECT pid FROM tl_page WHERE id=?)")->limit(1)->execute($id)) {
-            if ($parentPage->numRows > 0) return $parentPage;
-            else return false;
-        }
+        return Database::getInstance()->prepare("SELECT `id`, `alias`, `type` FROM tl_page WHERE id=(SELECT pid FROM tl_page WHERE id=?)")->limit(1)->execute($id);
+    }
+
+    public function requestPIDForID($id) {
+        $result = PageModel::findByPk($id);
+        if (!empty($result)) return $result->pid;
         return false;
     }
 
     public function getAliasForID($id) {
-        return Database::getInstance()->prepare("SELECT alias FROM tl_page WHERE id=?")->limit(1)->execute($id)->alias;
+        $page = PageModel::findById($id);
+        if (!empty($page)) return $page->alias;
+        else return false;
     }
 
     public function getDBFilePathForAlias($alias) {
-        return Database::getInstance()->prepare("SELECT `path` FROM tl_files WHERE `name`=? AND type = 'folder' AND `path` LIKE 'files/daten%'")->limit(1)->execute($alias);
+        $result = Database::getInstance()->prepare("SELECT `path` FROM tl_files WHERE `name`=? AND type = 'folder' AND `path` LIKE 'files/daten%'")->limit(1)->execute($alias);
+        if ($result->numRows > 0) return $result->path;
+        else return false;
+    }
+
+    public function pageExistsWithAlias($alias) {
+        return PageModel::countByAlias($alias) > 0;
     }
 }
--- src/Service/FileManager.php
+++ src/Service/FileManager.php
@@ -5,27 +5,47 @@ namespace GyWa\OrganizerBundle;
 use Contao\DataContainer;
 use Contao\CoreBundle\Exception\InternalServerErrorException;
 use Contao\Controller;
+use Contao\Files;
 use Contao\Input;
+use Psr\Log\LoggerInterface;
 
 class FileManager
 {
 
     private $dbManager;
+    private $files;
+    private $logger;
 
-    public function __construct(DatabaseManager $manager)
+    public function __construct(DatabaseManager $manager, LoggerInterface $logger)
     {
         $this->dbManager = $manager;
+        $this->files = Files::getInstance();
+        $this->logger = $logger;
+
+        if (\Input::get("key") == "adapt" || \Input::get("do") != "files") {
+            $this->checkForNecessaryFolders();
+        }
+
     }
 
-    public function createFolderStructureForAlias(DataContainer $dc, $oldAlias){
+    private function checkForNecessaryFolders() {
+        if (!$this->checkForFile(GyWaFileConfig::$rootFileDirectory)) {
+            throw new InternalServerErrorException('Folder "files/daten" does not exist in the current file-system or is not set to public. Please create the folder as well as the folder /files/trash for the GyWa-Organizer plugin to run.');
+        }
+        if (!$this->checkForFile(GyWaFileConfig::$trashFileDirectory)) {
+            throw new InternalServerErrorException('Folder "files/trash" does not exist in the current file-system or is not set to public. Please create the folder as well as the folder /files/daten for the GyWa-Organizer plugin to run.');
+        }
+    }
+
+    public function createFolderStructureForAlias(DataContainer $dc, $oldAlias) {
         if (!empty($oldAlias) && $oldAlias != $dc->activeRecord->alias) {
             $this->dbManager->syncFilesWithDatabase();
             $newFilePath = $this->getFilePathUpToPageId($dc->activeRecord->id, $dc->activeRecord->alias);
             $oldFilePath = $this->dbManager->getDBFilePathForAlias($oldAlias);
 
-            if ($oldFilePath->numRows > 0 && file_exists($oldFilePath->path)) {
-                if ($oldFilePath->path != $newFilePath) {
-                    $this->moveOrMergeFolders($oldFilePath->path, $newFilePath);
+            if (!empty($oldFilePath) && file_exists($oldFilePath)) {
+                if ($oldFilePath != $newFilePath) {
+                    $this->moveOrMergeFolders($oldFilePath, $newFilePath);
                 }
             }
             $this->dbManager->syncFilesWithDatabase();
@@ -34,9 +54,9 @@ class FileManager
             $newFilePath = $this->getFilePathUpToPageId($dc->activeRecord->id, $dc->activeRecord->alias);
             $oldFilePath = $this->dbManager->getDBFilePathForAlias($dc->activeRecord->alias);
 
-            if ($oldFilePath->numRows > 0 && file_exists($oldFilePath->path)) {
-                if ($oldFilePath->path != $newFilePath) {
-                    $this->moveOrMergeFolders($oldFilePath->path, $newFilePath);
+            if (!empty($oldFilePath) && file_exists($oldFilePath)) {
+                if ($oldFilePath != $newFilePath) {
+                    $this->moveOrMergeFolders($oldFilePath, $newFilePath);
                 }
             } else if (!$this->checkForFile($newFilePath)){
                 if (!mkdir($newFilePath, 0777, true)) {
@@ -49,35 +69,79 @@ class FileManager
 
     }
 
-    public function adaptFilesToPageStructure(DataContainer $dc) {
+    public function adaptFilesToPageStructure() {
         if (\Input::get("key") == "adapt") {
             try {
                 $this->dbManager->syncFilesWithDatabase();
-                $pages = $this->dbManager->selectAllRegularPages();
 
-                do {
-                    $newFilePath = $this->getFilePathUpToPageId($pages->id, $pages->alias);
-                    $oldFilePath = $this->dbManager->getDBFilePathForAlias($pages->alias);
-
-                    if ($oldFilePath->numRows > 0 && file_exists($oldFilePath->path)) {
-                        if ($oldFilePath->path != $newFilePath) {
-                            $this->moveOrMergeFolders($oldFilePath->path, $newFilePath);
-                        }
-                    } else if (!$this->checkForFile($newFilePath)){
-                        if (!mkdir($newFilePath, 0777, true)) {
-                            throw new InternalServerErrorException('New folder could not be created in ' . $newFilePath . '!');
-                        }
+                $duplicateFolders = $this->dbManager->checkForDistinctFileNames();
+                if (!empty($duplicateFolders) && $duplicateFolders->numRows > 0) {
+                    $folders = '';
+                    foreach($duplicateFolders->fetchAllAssoc() as $folder) {
+                        $folders .= $folder['path'] . ';';
                     }
-                } while ($pages = $pages->next());
+                    $this->logger->error('Duplicate folders in your file system: ');
+                    foreach($duplicateFolders->fetchAllAssoc() as $folder) {
+                        $this->logger->error($folder['path']);
+                    }
+                    throw new InternalServerErrorException('There are duplicate folders in your file-system! Please make sure every folder is named like the page alias it belongs to! A list of duplicate folders has been written to your log file.');
+                }
+
+                $pages = $this->dbManager->selectAllRegularPages();
+                if ($pages->numRows > 0) {
+                    do {
+                        $newFilePath = $this->getFilePathUpToPageId($pages->id, $pages->alias);
+                        $oldFilePath = $this->dbManager->getDBFilePathForAlias($pages->alias);
+
+                        $this->logger->info('Checking page ' . $pages->alias);
+
+                        if (!empty($oldFilePath) && file_exists($oldFilePath)) {
+                            $this->logger->info('Found old path ' . $oldFilePath . '. New path should be ' . $newFilePath . '.');
+                            if ($oldFilePath != $newFilePath) {
+                                $this->logger->info('Trying to move...');
+                                $this->moveOrMergeFolders($oldFilePath, $newFilePath);
+                            }
+                        } else if (!$this->checkForFile($newFilePath)){
+                            $this->logger->info('No old path found... creating new Path ' . $newFilePath . '.');
+                            if (!mkdir($newFilePath, 0777, true)) {
+                                throw new InternalServerErrorException('New folder could not be created in ' . $newFilePath . '!');
+                            }
+                        }
+
+                        $this->logger->info('Page ' . $pages->alias . ' completed!');
+                    } while ($pages->next());
+                    $this->dbManager->syncFilesWithDatabase();
+                }
+                unset($pages);
+
+                $this->cleanupFoldersAndFiles();
                 $this->dbManager->syncFilesWithDatabase();
+
             } catch (\Exception $exception) {
+                throw new InternalServerErrorException('Error occurred while adapting file structure: ' . $exception->getMessage());
             }
 
             Controller::redirect('contao?do=files');
         }
     }
 
-    public function organizeFiles(DataContainer $dc) {
+    private function cleanupFoldersAndFiles() {
+        $path = GyWaFileConfig::$rootFileDirectory;
+        $toRemove = array();
+        $rdi = new \RecursiveDirectoryIterator($path, \RecursiveDirectoryIterator::KEY_AS_PATHNAME | \RecursiveDirectoryIterator::SKIP_DOTS);
+        foreach (new \RecursiveIteratorIterator($rdi, \RecursiveIteratorIterator::CHILD_FIRST) as $file => $info) {
+            if ($info->isDir() && count(scandir($file)) == 2) {
+                array_push($toRemove, array('path' => $file, 'name' => $info->getFilename()));
+            }
+        }
+        foreach ($toRemove as $fileArray) {
+            if (file_exists($fileArray['path']) && !$this->dbManager->pageExistsWithAlias($fileArray['name'])) {
+                $this->files->rmdir($fileArray['path']);
+            }
+        }
+    }
+
+    public function organizeFiles() {
 
         if (Input::get('do') == 'page')
         {
@@ -95,7 +159,8 @@ class FileManager
 
                     if ($mode == 1) {
                         //Insert after newParent
-                        $newpID = \Database::getInstance()->prepare("SELECT pID FROM tl_page WHERE id=?")->limit(1)->execute($newParent)->pID;
+                        $newpID = $this->dbManager->requestPIDForID($newParent);
+
                         $this->resortFolder($pageID, $newpID);
                         unset($newpID);
                     } else if ($mode == 2) {
@@ -111,7 +176,7 @@ class FileManager
         }
     }
 
-    public function checkFileName($varValue, DataContainer $dc) {
+    public function checkFileName($varValue) {
         $varValue = str_replace(['_', '.', '/', '\\', ' '], '-', $varValue);
         $varValue = strtolower($varValue);
         if (preg_match('/^[a-z0-9-]+$/', $varValue)) {
@@ -129,21 +194,24 @@ class FileManager
                 throw new InternalServerErrorException('New folder could not be created in ' . $newPath . '!');
             }
         }
-        if (!rename($oldPath, $newPath)) {
+        if (!$this->files->rename($oldPath, $newPath)) {
             throw new InternalServerErrorException('Folder ' . $oldPath . ' could not be moved to new path ' . $newPath . '!');
         }
     }
 
     public function moveToTrash($path) {
-        $trashPath = GyWaFileConfig::$trashFileDirectory . date('Y-m-d-H-i-s') . '/' . substr($path, strlen(GyWaFileConfig::$rootFileDirectory), strlen($path) - strlen(GyWaFileConfig::$rootFileDirectory));
-        if (!mkdir($trashPath, 0777, true) || !rename($path, $trashPath)) {
+        $trashPath = GyWaFileConfig::$trashFileDirectory . date('Y-m-d-H-i-s') . substr($path, strlen(GyWaFileConfig::$rootFileDirectory), strlen($path) - strlen(GyWaFileConfig::$rootFileDirectory));
+
+        $this->logger->info('Moving ' . $path . ' to trash path ' . $trashPath);
+
+        if (!$this->files->rename($path, $trashPath)) {
             throw new InternalServerErrorException('Occupying folder in ' . $path . ' could not be moved to trash!');
         }
     }
 
     public function resortFolder($id, $newpid) {
         $pageToMove = $this->dbManager->getAliasForID($id);
-        $oldpid = $this->dbManager->requestParentPageForID($id);
+        $oldpid = $this->dbManager->requestPIDForID($id);
 
         if ($oldpid != $newpid) {
             $newParentPage = $this->dbManager->getAliasForID($newpid);
@@ -151,16 +219,13 @@ class FileManager
             $oldFilePath = $this->dbManager->getDBFilePathForAlias($pageToMove);
             $newFilePath = $this->dbManager->getDBFilePathForAlias($newParentPage);
 
-            if ($oldFilePath->numRows > 0) {
-                if ($this->checkForFile($oldFilePath->path)) { //If the old path exists
-                    if ($newFilePath->numRows == 0) { //The new file path does not exist (yet)
-                        $newPath = $this->createFilePathUpToID($newpid, $newParentPage->alias);
+            if (!empty($oldFilePath)) {
+                if ($this->checkForFile($oldFilePath)) { //If the old path exists
+                    if (empty($newFilePath)) { //The new file path does not exist (yet)
+                        $newPath = $this->createFilePathUpToID($newpid, $newParentPage);
                     } else {
-                        $newPath = $newFilePath->path;
-
-                        if (is_null($newPath) || empty($newPath)) {
-                            throw new InternalServerErrorException('No directory found for page alias ' . $pageToMove->alias . '.');
-                        } else if (!file_exists($newPath)) {
+                        $newPath = $newFilePath;
+                        if (!file_exists($newPath)) {
                             //The new file path does exist in the database but not in the file system
                             if(!mkdir(strtolower($newPath), 0777, true)) {
                                 throw new InternalServerErrorException('Directory ' . $newPath . ' cannot be created or accessed.');
@@ -169,8 +234,8 @@ class FileManager
                     }
 
                     if ($this->checkForFile($newPath)) { //Both paths exist and can be written to!
-                        if ($oldFilePath->path != ($newPath . '/' . $pageToMove)) {
-                            $this->moveOrMergeFolders($oldFilePath->path, $newPath . '/' . $pageToMove);
+                        if ($oldFilePath != ($newPath . '/' . $pageToMove)) {
+                            $this->moveOrMergeFolders($oldFilePath, $newPath . '/' . $pageToMove);
                         }
                     } else {
                         throw new InternalServerErrorException('Directory ' . $newPath . ' cannot be accessed.');
@@ -178,23 +243,20 @@ class FileManager
                 }
             }
 
-            unset($oldFilePath, $newFilePath);
         }
 
     }
 
     public function checkForFile($path) {
-        return (!empty($path) && file_exists($path) && is_dir($path) && is_writable($path));
+        return (!empty($path) && file_exists($path) && is_dir($path) && $this->files->is_writeable($path));
     }
 
     public function getFilePathUpToPageId($id, $startAlias) {
-        $path = '';
-        $alias = $startAlias;
+        $path = '/' . $startAlias;
 
-        while(($parent = $this->dbManager->requestParentPageForID($id)) && $parent->type != 'root') {
-            $path = '/' . $alias . $path;
+        while(!empty(($parent = $this->dbManager->requestParentPageForID($id))) && $parent->type != 'root') {
+            $path = '/' . $parent->alias . $path;
             $id = $parent->id;
-            $alias = $parent->alias;
         }
 
         $path = GyWaFileConfig::$rootFileDirectory . $path;
@@ -215,8 +277,8 @@ class FileManager
     public function removeFolderForAlias(DataContainer $dc) {
         $this->dbManager->syncFilesWithDatabase();
         $filePath = $this->dbManager->getDBFilePathForAlias($dc->activeRecord->alias);
-        if ($filePath->numRows > 0 && $this->checkForFile($filePath->path)) {
-            $this->moveToTrash($filePath->path);
+        if (!empty($filePath) && $this->checkForFile($filePath)) {
+            $this->moveToTrash($filePath);
         }
         $this->dbManager->syncFilesWithDatabase();
     }
